a35a4adbfe3ac2cd1b4d0df27d03f957982c35cc
Jasper <japsu.honkasalo@gmail.com>
2024-05-24 14:06:54 +0300

    WIP enemy attack phase

---
+++ MVP/Multiplayer Card Game MVP/Assets/Code/Scripts/PhaseSystem/Enemies/Sequential/EnemyAttackPhase.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using Entities.Enemies;
+using UnityEngine;
+
+namespace PhaseSystem.Enemies.Sequential
+{
+    [CreateAssetMenu(menuName = "Phases/Enemies/Sequential/Attack", fileName = "Attack", order = 0)]
+    public class EnemyAttackPhase : SequentialPhase<EnemyCharacter>
+    {
+        protected override IEnumerator OnExecute(EnemyCharacter instance)
+        {
+            yield return null;
+        }
+    }
+}
