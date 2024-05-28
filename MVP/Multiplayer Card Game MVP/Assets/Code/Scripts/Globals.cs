db2c61e680a53a733b09c50089d4b83f4514571c
Jasper <japsu.honkasalo@gmail.com>
2024-05-28 10:27:52 +0300

    Add comments

--- MVP/Multiplayer Card Game MVP/Assets/Code/Scripts/Globals.cs
+++ MVP/Multiplayer Card Game MVP/Assets/Code/Scripts/Globals.cs
@@ -1,4 +1,7 @@
-﻿public static class Globals
+﻿/// <summary>
+/// Provides a place to store global variables that can be accessed from any script.
+/// </summary>
+public static class Globals
 {
     public static bool VerboseEnemyMovement = false;
 }
