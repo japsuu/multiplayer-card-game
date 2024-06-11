0490409a8f79dc270aca37249bc06943b1eb2cbd
Jasper <japsu.honkasalo@gmail.com>
2024-06-11 10:23:10 +0300

    Add ActionChoicePanel to easily query for player choices

---
+++ MVP/Multiplayer Card Game MVP/Assets/Code/Scripts/UI/ChoicePanel/ActionChoicePanel.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using StateManagement;
+using UnityEngine;
+
+namespace UI.ChoicePanel
+{
+    public class ActionChoicePanel : MonoBehaviour
+    {
+        [SerializeField]
+        private ActionChoiceButton _buttonPrefab;
+        
+        [SerializeField]
+        private Transform _buttonContainer;
+        
+        [SerializeField]
+        private GameObject _content;
+
+
+        private void Awake()
+        {
+            _content.SetActive(false);
+        }
+
+
+        private void OnEnable()
+        {
+            GameState.OnRequestChoice += OnRequestChoice;
+        }
+        
+        
+        private void OnDisable()
+        {
+            GameState.OnRequestChoice -= OnRequestChoice;
+        }
+
+
+        private void OnRequestChoice(string[] choices, Action<string> callback)
+        {
+            _content.SetActive(true);
+            
+            foreach (Transform child in _buttonContainer)
+            {
+                Destroy(child.gameObject);
+            }
+
+            foreach (string choice in choices)
+            {
+                ActionChoiceButton button = Instantiate(_buttonPrefab, _buttonContainer);
+                button.Initialize(choice, c => OnChoiceSelected(c, callback));
+            }
+        }
+        
+        
+        private void OnChoiceSelected(string choice, Action<string> callback)
+        {
+            callback(choice);
+            _content.SetActive(false);
+        }
+    }
+}
---
+++ MVP/Multiplayer Card Game MVP/Assets/Code/Scripts/UI/ShowHideUIButton.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public class ShowHideUIButton : MonoBehaviour
+    {
+        [SerializeField]
+        private Button _button;
+        
+        public GameObject Target;
+        
+        private bool _isVisible;
+        
+        
+        private void Awake()
+        {
+            _button.onClick.AddListener(OnPressed);
+        }
+
+
+        private void OnPressed()
+        {
+            Target.SetActive(!_isVisible);
+        }
+    }
+}
